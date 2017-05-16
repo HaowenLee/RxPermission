20802341eccb5bdd937c3d1a4588689c79bbe49e
lhw <lihaowen@yiyahanyu.com>
2017-05-16 11:26:25 +0800

    add notes of Permission

--- app/src/main/java/com/rxpermission/Permission.kt
+++ app/src/main/java/com/rxpermission/Permission.kt
@@ -1,3 +1,8 @@
 package com.rxpermission
 
+/**
+ * @param name The name of the permission
+ * @param granted If the permission has been granted to the given package.
+ * @param shouldShowRequestPermissionRationale Gets whether you should show UI with rationale for requesting a permission.
+ */
 data class Permission(val name: String, val granted: Boolean, val shouldShowRequestPermissionRationale: Boolean)
